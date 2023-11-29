7e3ec9e57ac476fa720a577dcc22ca9d67089668
Robert Wilcox <42777835+stampylongr@users.noreply.github.com>
2023-11-29 14:36:49 +0000

    fix: pass arguments to `launch()` in `App.java`

--- src/main/java/kagemori/michiru/App.java
+++ src/main/java/kagemori/michiru/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     private static Scene scene;
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
     @Override
